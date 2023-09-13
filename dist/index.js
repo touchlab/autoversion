721eba226be695bf082b677565bb48f378bec1a9
Kevin Galligan <kgalligan@gmail.com>
2023-09-12 21:21:47 -0400

    Delete marker tags

--- dist/index.js
+++ dist/index.js
@@ -11626,26 +11626,38 @@ const TEMP_PUBLISH_PREFIX = "autoversion-tmp-publishing-";
 async function run() {
     try {
         const versionBase = core.getInput('versionBase');
+        const removeTempPublish = core.getInput('removeTempPublish');
         core.debug(`versionBase: ${versionBase}`);
+        core.debug(`removeTempPublish: ${removeTempPublish}`);
         const git = (0, simple_git_1.default)();
         const tags = await git.tags();
         const versionBaseCompare = `${versionBase}.`;
         core.debug('----------tags----------');
         tags.all.forEach(t => core.debug(t));
         core.debug('----------tags----------');
-        const matching = tags.all
-            .map(t => t.startsWith(TEMP_PUBLISH_PREFIX) ? t.substring(TEMP_PUBLISH_PREFIX.length) : t)
-            .filter(t => t.startsWith(versionBaseCompare))
-            .map(t => preload_1.default.parse(t))
-            .filter(ver => ver !== null && ver !== undefined);
-        const sorted = matching.sort((v1, v2) => v2.compare(v1));
-        const nextPatch = sorted.length > 0 ? sorted[0].patch + 1 : 0;
-        const nextVersion = `${versionBase}.${nextPatch}`;
-        // Set outputs for other workflow steps to use
-        core.setOutput('nextVersion', nextVersion);
-        const markerTag = `${TEMP_PUBLISH_PREFIX}${nextVersion}`;
-        await git.raw(["tag", markerTag]);
-        await git.raw(["push", "origin", "tag", markerTag]);
+        if (removeTempPublish === '') {
+            const matching = tags.all
+                .map(t => t.startsWith(TEMP_PUBLISH_PREFIX) ? t.substring(TEMP_PUBLISH_PREFIX.length) : t)
+                .filter(t => t.startsWith(versionBaseCompare))
+                .map(t => preload_1.default.parse(t))
+                .filter(ver => ver !== null && ver !== undefined);
+            const sorted = matching.sort((v1, v2) => v2.compare(v1));
+            const nextPatch = sorted.length > 0 ? sorted[0].patch + 1 : 0;
+            const nextVersion = `${versionBase}.${nextPatch}`;
+            // Set outputs for other workflow steps to use
+            core.setOutput('nextVersion', nextVersion);
+            const markerTag = `${TEMP_PUBLISH_PREFIX}${nextVersion}`;
+            await git.raw(["tag", markerTag]);
+            await git.raw(["push", "origin", "tag", markerTag]);
+        }
+        else {
+            const markerTagPrefix = `${TEMP_PUBLISH_PREFIX}${versionBase}`;
+            const tagsToDelete = tags.all.filter(t => t.startsWith(markerTagPrefix));
+            await Promise.all(tagsToDelete.map(async (t) => {
+                await git.raw(["tag", "-d", t]);
+                await git.raw(["push", "origin", "-d", markerTagPrefix]);
+            }));
+        }
     }
     catch (error) {
         // Fail the workflow run if an error occurs
--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,10 @@ const TEMP_PUBLISH_PREFIX = "autoversion-tmp-publishing-"
 export async function run(): Promise<void> {
   try {
     const versionBase: string = core.getInput('versionBase')
+    const removeTempPublish: string = core.getInput('removeTempPublish')
 
     core.debug(`versionBase: ${versionBase}`)
+    core.debug(`removeTempPublish: ${removeTempPublish}`)
 
     const git = simpleGit();
     const tags = await git.tags()
@@ -20,22 +22,34 @@ export async function run(): Promise<void> {
     core.debug('----------tags----------')
     tags.all.forEach(t => core.debug(t))
     core.debug('----------tags----------')
-    const matching = tags.all
-        .map(t => t.startsWith(TEMP_PUBLISH_PREFIX) ? t.substring(TEMP_PUBLISH_PREFIX.length) : t)
-        .filter(t => t.startsWith(versionBaseCompare))
-        .map(t => semver.parse(t))
-        .filter(ver => ver !== null && ver !== undefined)
 
-    const sorted = matching.sort((v1, v2) => v2!.compare(v1!))
-    const nextPatch = sorted.length > 0 ? sorted[0]!.patch + 1 : 0
-    const nextVersion = `${versionBase}.${nextPatch}`
+    if (removeTempPublish === '') {
+      const matching = tags.all
+          .map(t => t.startsWith(TEMP_PUBLISH_PREFIX) ? t.substring(TEMP_PUBLISH_PREFIX.length) : t)
+          .filter(t => t.startsWith(versionBaseCompare))
+          .map(t => semver.parse(t))
+          .filter(ver => ver !== null && ver !== undefined)
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('nextVersion', nextVersion)
+      const sorted = matching.sort((v1, v2) => v2!.compare(v1!))
+      const nextPatch = sorted.length > 0 ? sorted[0]!.patch + 1 : 0
+      const nextVersion = `${versionBase}.${nextPatch}`
 
-    const markerTag = `${TEMP_PUBLISH_PREFIX}${nextVersion}`;
-    await git.raw(["tag", markerTag])
-    await git.raw(["push", "origin", "tag", markerTag])
+      // Set outputs for other workflow steps to use
+      core.setOutput('nextVersion', nextVersion)
+
+      const markerTag = `${TEMP_PUBLISH_PREFIX}${nextVersion}`;
+
+      await git.raw(["tag", markerTag])
+      await git.raw(["push", "origin", "tag", markerTag])
+    } else {
+      const markerTagPrefix = `${TEMP_PUBLISH_PREFIX}${versionBase}`;
+      const tagsToDelete = tags.all.filter(t => t.startsWith(markerTagPrefix))
+
+      await Promise.all(tagsToDelete.map(async (t) => {
+        await git.raw(["tag", "-d", t])
+        await git.raw(["push", "origin", "-d", markerTagPrefix])
+      }))
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
